12edfa9b4f1e0acb3033150f6a42e2165ad782a3
JoHE64 <161314911+JoHE64@users.noreply.github.com>
2024-05-13 10:00:10 -0300

    oi

---
+++ LISTA 02 - JS/funcao.js
@@ -0,0 +1,22 @@
+function exercicio01(){
+    const var01 = document.getElementById("VarEx01").value;
+    const var02 = document.getElementById("VarEx02").value;
+    const calcular = document.getElementById("calcular").innerText;
+    
+    let diferenca = var01 - var02;
+    alert(diferenca);
+
+    let dobro = ((var01 * 2) + (var02*3));
+
+    let multiplicacao = (var01 * var02);
+
+    if(let = maiorMenor){
+        var01 > var02;
+        maiorMenor = var01 , var02;
+    }else{
+        maiorMenor = var02 , var01;
+    };
+
+    alert(var01);
+
+}
